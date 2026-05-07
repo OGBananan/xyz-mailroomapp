f48b7049d925aa5289b42ae4d1e01a66076a77ad
Anurag Bansal <anurag2003bansal@gmail.com>
2026-05-07 23:00:00 +0530

    refactor: remove obsolete Terraform bootstrap configuration files

--- infra/bootstrap/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "state_bucket" {
-  value       = aws_s3_bucket.state.bucket
-  description = "Paste this into envs/*/versions.tf backend.bucket"
-}
-
-output "lock_table" {
-  value       = aws_dynamodb_table.lock.name
-  description = "Paste this into envs/*/versions.tf backend.dynamodb_table"
-}
